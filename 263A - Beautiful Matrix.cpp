01c87a09c0b14ce55a07bd26397949daeefdc3ec
Diego Villegas <diego.apoloniov@gmail.com>
2022-09-26 16:07:11 -0500

    Update 263A - Beautiful Matrix.cpp

--- 263A - Beautiful Matrix.cpp
+++ 263A - Beautiful Matrix.cpp
@@ -1,4 +1,4 @@
-// 170132154 | Aug/30/2022 02:41 UTC-5 | diegolonio | 263A - Beautiful Matrix | GNU C++14 | Accepted | 30 ms | 0 KB
+// 173109909 | Sep/22/2022 14:53 UTC-5 | diegolonio | 263A - Beautiful Matrix | GNU C++14 | Accepted | 30 ms | 0 KB
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,15 +14,15 @@ int main()
         cin >> row[0] >> row[1] >> row[2] >> row[3] >> row[4];
 
         if (row[0]) {
-            cout << abs(3 - (5-i)) + 2 << "\n";
+            cout << abs(i - 2) + 2 << "\n";
         } else if (row[1]) {
-            cout << abs(3 - (5-i)) + 1 << "\n";
+            cout << abs(i - 2) + 1 << "\n";
         } else if (row[2]) {
-            cout << abs(3 - (5-i)) + 0 << "\n";
+            cout << abs(i - 2) + 0 << "\n";
         } else if (row[3]) {
-            cout << abs(3 - (5-i)) + 1 << "\n";
+            cout << abs(i - 2) + 1 << "\n";
         } else if (row[4]) {
-            cout << abs(3 - (5-i)) + 2 << "\n";
+            cout << abs(i - 2) + 2 << "\n";
         }
     }
 
